0304bde5582ecce17630278636acca9cf56d51d7
Brent Vatne <brentvatne@gmail.com>
2021-06-23 12:36:04 -0700

    ok

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import * as Linking from "expo-linking";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{Linking.createURL("")}</Text>
+      <Text>scheme: {Constants.manifest.scheme}</Text>
+      <Text>Version {Constants.manifest.version}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "cyan",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
   },
 });
